a6d0e1eb28758e7b22ba13d5e36cb67f5504eb8f
jamie <jameiapps101@gmail.com>
2022-02-28 18:16:42 +0000

    added ability to control pump output (currently open mosfet)

--- monitor/src/actuation.rs
+++ monitor/src/actuation.rs
@@ -4,7 +4,8 @@ use rppal::gpio::OutputPin;
 
 pub struct Gpio {
     dev: rppal_gpio,
-    led_pin: OutputPin
+    led_pin: OutputPin,
+    pump_pin: OutputPin
 }
 
 #[derive(Debug,Deserialize,Serialize)]
@@ -19,15 +20,20 @@ impl Gpio {
     pub fn new() -> Self { 
         let dev = rppal_gpio::new().unwrap(); 
         let led_pin = dev.get(5).unwrap().into_output();
-        Self { dev, led_pin } 
+        let pump_pin = dev.get(13).unwrap().into_output();
+        Self { dev, led_pin, pump_pin } 
     }
 
     pub fn set(&mut self, c: Command) {
-        println!("Setting pin {} {:?}",c.gpio,c.action);
+        println!("Setting pin {:?} {:?}",c.output,c.action);
+        let output_ref = match c.output {
+            GpioOutput::Led  => &mut (self.led_pin),
+            GpioOutput::Pump => &mut (self.pump_pin),
+        };
         match c.action {
-            GpioAction::On => self.led_pin.set_high(),
-            GpioAction::Off  => self.led_pin.set_low(),
-            GpioAction::Toggle => self.led_pin.toggle(),
+            GpioAction::On     => output_ref.set_high(),
+            GpioAction::Off    => output_ref.set_low(),
+            GpioAction::Toggle => output_ref.toggle(),
         }
         
     }
@@ -36,7 +42,7 @@ impl Gpio {
 #[derive(Deserialize,Serialize)]
 #[serde(crate = "common::serde")]
 pub struct Command {
-    gpio: u8, 
+    output: GpioOutput, 
     action: GpioAction,
 }
 
